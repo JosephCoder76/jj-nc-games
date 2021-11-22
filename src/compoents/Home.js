cc06163d005bfc0df6a57ccc4c0194ac02d77087
josephcoder76 <chenstyle@talk21.com>
2021-11-22 07:39:07 +0000

    Completed The Bolider Plate Setup for me FrontEnd React Project

---
+++ src/compoents/Home.js
@@ -0,0 +1,12 @@
+import HomeGif from '../images/HomePage.gif'
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>Welcome to NC Marketplace!</h1>
+      <img src={HomeGif} alt='Store'></img>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/utils/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+// const marketApi = axios.create({
+//   baseURL: `https://nc-marketplace.herokuapp.com/api`
+// })
+
+// export const getAllItems = () => {
+//   return marketApi.get(`/items`).then(res => res.data.items)}
+
+// export const postItem = (itemToSell) => {
+//   return marketApi.post(`/items`, itemToSell).then(res => res.data.item)}
+
+// export const deleteItem = (itemId) => {
+//   return marketApi.delete(`/items`, itemId).then(res => res.data.item)}
+
+// export const getAllCategories = () => {
+//   return marketApi.get(`/categories`).then(res => res.data.categories)}
+
+// export const getCategoryByName = (name) => {
+//     return marketApi.get(`/categories/${name}`).then(res => res.data.category)}
+
+// export const getUsers = () => {
+//   return marketApi.get(`/users`).then(res => res.data.users)}
+
+// export const getUserByName = (name) => {
+//   return marketApi.get(`/users/${name}`).then(res => res.data.user)}
+
+
